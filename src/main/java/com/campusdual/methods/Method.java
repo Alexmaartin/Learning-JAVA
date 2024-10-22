3e2ca3967867b51ea29c53a8548e9eb9f2407919
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-10-22 18:52:56 +0200

    WIP

---
+++ src/main/java/com/campusdual/methods/Method.java
@@ -0,0 +1,39 @@
+package com.campusdual.methods;
+
+public class Method {
+        public static int arithmeticOperators(){
+                int value = 5;                      //5
+                value = value + 3;                  //8
+                value = 2 - value;                   //-6
+                value += 3;
+                value -= 1;
+                value *= 4;
+                value = (value - 2) / 2;
+                int newValue = 3;
+                int myValue = 2;
+                value = newValue + myValue;
+                final int CONST = 2;
+                value = CONST;
+                value += 3;
+                value = value % 2;
+                value++;
+                value--;
+               return value;
+        }
+    public static void logicOperators(){
+        System.out.println(" 4 is equal to 4;" + (4==4));
+        System.out.println(" 3 is different to 4;"+ (3!=4));
+        System.out.println(" 3 is greater than 5;"+ (3>5));
+        System.out.println(" 5 is less or equal than 5;"+ (5<=5));
+        System.out.println("Unique true combination for AND;"+ (true && true));
+        System.out.println("Unique false combination for or;"+ (false || false));
+        System.out.println("NOT false; "+ (!false));
+    }
+
+    public static void main(String[] args) {
+        logicOperators();
+        int arithResult = arithmeticOperators();
+        System.out.println("Aritmetic result;"+ arithmeticOperators());
+    }
+    }
+
