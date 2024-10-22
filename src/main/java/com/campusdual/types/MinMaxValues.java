d6439f3020a2c938b3b937f3a7c3bdc7b2a6a17a
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-10-22 17:15:22 +0200

    Cast

---
+++ src/main/java/com/campusdual/types/MinMaxValues.java
@@ -0,0 +1,19 @@
+package com.campusdual.types;
+
+public class MinMaxValues {
+    public static void main(String[] args) {
+        System.out.println("Minimum byte value;"+Byte.MIN_VALUE);
+        System.out.println("Maximum byte value;"+Byte.MAX_VALUE);
+        System.out.println("Minimum short value;"+Short.MIN_VALUE);
+        System.out.println("Maximum short value;"+Short.MAX_VALUE);
+        System.out.println("Minimum integer value;"+Integer.MIN_VALUE);
+        System.out.println("Maximum integer value;"+Integer.MAX_VALUE);
+        System.out.println("Minimun long value;"+Long.MIN_VALUE);
+        System.out.println("Maximun long value;"+Long.MAX_VALUE);
+        System.out.println("Minimun float value"+Float.MIN_VALUE);
+        System.out.println("Maximun float value"+Float.MAX_VALUE);
+        System.out.println("Minimun double value"+Double.MIN_VALUE);
+        System.out.println("Maximun double value"+Double.MAX_VALUE);
+
+    }
+}
