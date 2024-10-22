07c9a4380bd128c90b8f9cf0f4299f7e5c0c7ec0
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-10-22 16:15:50 +0200

    Terminado ejercicio de variables y tipos

---
+++ src/main/java/com/campusdual/variables/Variable.java
@@ -0,0 +1,17 @@
+package com.campusdual.variables;
+
+import com.sun.jdi.NativeMethodException;
+
+import javax.naming.Name;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Variable {
+    public static void main(String[] args) {
+        double  hours = 2.5;
+        int month = 12;
+        String name = "Ramiro";
+        final int DAY_HOURS = 24;
+        name = "Alex";
+        System.out.println(name);
+    }
+}
