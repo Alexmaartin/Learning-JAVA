23b9636cb5ad40ce9f1bbd6204cb6ccc395fde5b
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-10-23 16:01:51 +0200

    WIP

---
+++ src/main/java/com/campusdual/statics/StaticMethod.java
@@ -0,0 +1,16 @@
+package com.campusdual.statics;
+
+//import java.util.Calendar;
+//import java.util.Date;
+//
+//public class StaticMethod {
+//
+//    public static String countDownToYear(String name, int year){
+//        Date today = new Date();
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(year,0,1,0,0,0);
+//        Date targetDate = calendar.getTime();
+//    }
+//
+//    }
+//}
